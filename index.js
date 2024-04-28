c0e07de9b40ce93b0186986a1b8cee703fb7e2c2
joetroll <joetroll@proton.me>
2024-04-28 20:31:56 -0300

    scary new fallout theme with janky typewriter effect

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+// i'm sorry for what you have to witness
 // makes it so the links open in a new tab
 document.addEventListener('DOMContentLoaded', function() {
   const links = document.querySelectorAll('.icons a');
+  const text = "joetroll";
+  const speed = 100;
+  const typingArea = document.getElementById('typewriter');
+  let i = 0;
+
+  typingArea.style.visibility = 'visible'; // make the typewriter effect actually visible
+  typingArea.innerHTML = ''; // deletes the first line of text
+
+  function typeWriter() {
+    if (i < text.length) {
+      typingArea.innerHTML += text.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  }
+
+  typeWriter();
+});
 
   links.forEach(link => {
     link.addEventListener('click', function(event) {
@@ -8,4 +27,3 @@ document.addEventListener('DOMContentLoaded', function() {
       window.open(this.href, '_blank');
     });
   });
-});
