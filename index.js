98b332912b05798096a030ba45daae549bc0a4f8
joetroll <joetroll@proton.me>
2024-05-01 22:11:43 -0300

    add option to remove animations by using my lack of js skills, change to status badge

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // i'm sorry for what you have to witness
+
+const animationCheckbox = document.getElementById('animationCheckbox');
+
 document.addEventListener('DOMContentLoaded', function() {
   var app = document.getElementById('app');
 
@@ -10,4 +13,21 @@ document.addEventListener('DOMContentLoaded', function() {
     .start();
 
 
+    });
+
+    animationCheckbox.addEventListener('change', function() {
+      const elementsWithAnimation = document.querySelectorAll('.crt, .container');
+      if (this.checked) {
+        elementsWithAnimation.forEach(element => {
+          element.style.animation = 'none';
+          element.style.textShadow = 'none';
+          element.classList.add('animations-stopped');
+        });
+      } else {
+        elementsWithAnimation.forEach(element => {
+          element.style.removeProperty('animation');
+          element.style.removeProperty('text-shadow');
+          element.classList.remove('animations-stopped');
+        });
+      }
     });
